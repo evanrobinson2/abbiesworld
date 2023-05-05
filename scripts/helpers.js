d11ed013f2c4a7fd7f34997bffd7eb790052a3fa
evanrobinson2 <evanr2@gmail.com>
2023-05-05 01:14:38 -0400

    New version with clouds floating and better physical controls. Up next: particle emitter.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,10 +1,10 @@
 export function updateShipVelocity(ship, keyA, keyD, keyW, keyS, keySpace) {
     if (keyA.isDown) {
       // Rotate the ship counter-clockwise by 15 degrees per update
-      ship.rotation -= Phaser.Math.DegToRad(15);
+      ship.rotation -= Phaser.Math.DegToRad(1.5);
     } else if (keyD.isDown) {
       // Rotate the ship clockwise by 15 degrees per update
-      ship.rotation += Phaser.Math.DegToRad(15);
+      ship.rotation += Phaser.Math.DegToRad(1.5);
     }
   
     if (keyW.isDown) {
@@ -16,16 +16,16 @@ export function updateShipVelocity(ship, keyA, keyD, keyW, keyS, keySpace) {
     } else if (keyS.isDown) {
       // Calculate the velocity vector based on the ship's rotation
       const velocityX = Math.cos(ship.rotation - Phaser.Math.DegToRad(25));
-      const velocityY = Math.sin(ship.rotation - Phaser.Math.DegToRad(25));
+      const velocityY = Math.sin(ship.rotation - Phaser.Math.DegToRad(5));
       ship.body.velocity.x -= velocityX;
       ship.body.velocity.y -= velocityY;
     }
   
     if (keySpace.isDown) {
       // Reduce velocity by 5 units per update while spacebar is held down
-      ship.body.velocity.x -= 5;
+      ship.body.velocity.x -= 1;
       if (ship.body.velocity.x < 0) ship.body.velocity.x = 0;
-      ship.body.velocity.y -= 5;
+      ship.body.velocity.y -= 1;
       if (ship.body.velocity.y < 0) ship.body.velocity.y = 0;
     }
   }
@@ -39,11 +39,13 @@ export function updateShipRotation(ship, keyQ, keyE) {
 }
 
 export function updateShipScale(ship, keyDown, keyUp) {
+    const scaleFactor = 0.01; // Percentage scale factor
+  
     if (keyDown.isDown) {
-        ship.setScale(ship.scaleX - 0.05, ship.scaleY - 0.05);
+      ship.setScale(ship.scaleX * (1 - scaleFactor), ship.scaleY * (1 - scaleFactor));
     } else if (keyUp.isDown) {
-        ship.setScale(ship.scaleX + 0.05, ship.scaleY + 0.05);
+      ship.setScale(ship.scaleX * (1 + scaleFactor), ship.scaleY * (1 + scaleFactor));
     }
-}
-
+  }
+  
   
--- scripts/main.js
+++ scripts/main.js
@@ -31,10 +31,33 @@ let keyQ;
 let keyE;
 let keyDown;
 let keyUp;
+let cloudGroup; // Declare cloudGroup as a global variable
+
+
+const spriteWidth = 146;
+const spriteHeight = 146;
+const numFrames = 49;
+const numColumns = 7;
+
+
+
 
 function preload() {
   this.load.image('worldmap', 'assets/images/1.png');
   this.load.image('ship', 'assets/images/ship.png');
+  this.load.image('cloud1', 'assets/images/cloud1.png');
+  this.load.image('cloud2', 'assets/images/cloud2.png');
+  this.load.image('cloud3', 'assets/images/cloud3.png');
+  this.load.image('cloud4', 'assets/images/cloud4.png');
+
+  this.load.spritesheet('exhaust', 'assets/images/exhaust.png', {
+    frameWidth: spriteWidth,
+    frameHeight: spriteHeight,
+    startFrame: 0,
+    endFrame: numFrames - 1,
+    margin: 0,
+    spacing: 0
+  });
 }
 
 function create() {
@@ -44,6 +67,7 @@ function create() {
 
   ship = this.physics.add.sprite(900, 500, 'ship');
   ship.setScale(0.35);
+  ship.setDepth(1);
 
   cursors = this.input.keyboard.createCursorKeys();
   keyA = this.input.keyboard.addKey('A');
@@ -55,10 +79,68 @@ function create() {
   keyE = this.input.keyboard.addKey('E');
   keyDown = this.input.keyboard.addKey('DOWN');
   keyUp = this.input.keyboard.addKey('UP');
+
+  const cloudImages = ['cloud1', 'cloud2', 'cloud3', 'cloud4']; // Replace with your cloud image names
+  const totalClouds = 8; // Adjust the number of clouds as desired
+
+  cloudGroup = createFloatingClouds(this, cloudImages, totalClouds);
+
+  const sprites = this.anims.generateFrameNames('exhaust', {
+    start: 0,
+    end: numFrames - 1,
+    zeroPad: 0,
+    prefix: '',
+    suffix: ''
+  });
+
+  // Create sprites using the frames
+  sprites.forEach((sprite, index) => {
+    const x = (index % numColumns) * spriteWidth;
+    const y = Math.floor(index / numColumns) * spriteHeight;
+
+    const exhaustSprite = this.add.sprite(x, y, 'exhaust', sprite.frame);    
+    exhaustSprite.setVisible(false);
+  });
 }
 
 function update() {
-  updateShipVelocity(ship, keyA, keyD, keyW, keyS, keySpace);
+  updateShipVelocity(ship, keyA, keyD, keyS, keyW, keySpace);
   updateShipRotation(ship, keyQ, keyE);
   updateShipScale(ship, keyDown, keyUp);
+
+  // Iterate through each cloud in the cloud group
+  cloudGroup.getChildren().forEach(cloud => {
+    // Apply gentle horizontal movement
+    cloud.x += Math.sin(cloud.y / 100) * 0.5; // Adjust the 0.5 value to control the horizontal movement speed
+
+    // Wrap the clouds when they go off-screen
+    if (cloud.x > config.width + cloud.displayWidth) {
+      cloud.x = -cloud.displayWidth;
+    } else if (cloud.x < -cloud.displayWidth) {
+      cloud.x = config.width + cloud.displayWidth;
+    }
+  });
+}
+
+export function createFloatingClouds(scene, cloudImages, totalClouds) {
+  const cloudGroup = scene.physics.add.group(); // Create a group to hold the clouds
+
+  // Generate the specified number of clouds
+  for (let i = 0; i < totalClouds; i++) {
+    const cloudImage = Phaser.Utils.Array.GetRandom(cloudImages); // Get a random cloud image
+    const x = Phaser.Math.Between(0, scene.sys.game.config.width); // Random X position
+    const y = Phaser.Math.Between(0, scene.sys.game.config.height); // Random Y position
+    const scale = Phaser.Math.FloatBetween(0.5, 1.5); // Random scale factor (between 0.5 and 1.5)
+    const alpha = Phaser.Math.FloatBetween(0.2, 0.8); // Random transparency (between 0.2 and 0.8)
+
+    const cloud = scene.add.image(x, y, cloudImage)
+      .setScale(scale)
+      .setAlpha(alpha)
+      .setDepth(4); // Set the cloud's depth (above the background image)
+
+    // Add the cloud to the physics group
+    cloudGroup.add(cloud);
+  }
+
+  return cloudGroup; // Return the cloud group for further manipulation if needed
 }
