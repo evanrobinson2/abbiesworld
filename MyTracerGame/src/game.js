7406358fd494d07e9bc2de8565d894614bcb9dbd
evanrobinson2 <evanr2@gmail.com>
2023-05-27 09:20:53 -0400

    added proper ARd images of the unicorn workshop

--- MyTracerGame/src/game.js
+++ MyTracerGame/src/game.js
@@ -1,6 +1,9 @@
-var gameWidth = 800;
-var gameHeight = 600;
+var gameWidth = 1232;
+var gameHeight = 923;
 var player;
+var goon1;
+var goon2;
+var prizePic;
 var scene;
 var cursors;
 var lineGraphics;
@@ -9,6 +12,11 @@ var numSegments = 0;
 var uiText;
 var recentDirection = null;
 var isPaused = false;
+var background;
+var prize;
+var goonSpeed = 200; // Initial speed of the goons
+var goonRotationSpeed = 0.1; // Rotation speed multiplier
+var goonSpeedIncrease = 0.001; // Speed increase on bounce
 
 var config = {
     type: Phaser.AUTO,
@@ -23,7 +31,8 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 } // Set gravity to zero
+            gravity: { y: 0 },
+            debug: false // Set to true for debugging collision boundaries
         }
     }
 };
@@ -32,20 +41,57 @@ var game = new Phaser.Game(config);
 
 function preload() {
     // Preload assets here
+    this.load.image('prize1', 'assets/images/prize1.png');
+    this.load.image('prize2', 'assets/images/prize2.png');
+    this.load.image('prize3', 'assets/images/prize3.png');
+    this.load.image('prize4', 'assets/images/prize4.png');
     this.load.image('player', 'assets/images/player.png');
+    this.load.image('goon1', 'assets/images/goon1.png');
+    this.load.image('goon2', 'assets/images/goon2.png');
     this.load.image('starfield', 'assets/images/starfield.png');
 }
 
 function create() {
     // Create game objects and initialize the game here
     scene = this;
-    scene.add.image(0, 0, 'starfield').setOrigin(0);
+
+    // Randomly select a prize image
+    var prizeImages = ['prize1', 'prize2', 'prize3', 'prize4'];
+    var randomPrizeImage = Phaser.Math.RND.pick(prizeImages);
+
+    // Create background image
+    background = scene.add.image(0, 0, 'starfield').setOrigin(0);
+                     
+    // Create prize image
+    prize = scene.add.image(0, 0, randomPrizeImage).setOrigin(0);
+
+    // Set depth for proper layering
+    background.setDepth(0);
+    prize.setDepth(1);
+
+    // Register 'F' key input for changing the order of the images
+    scene.input.keyboard.on('keydown-F', toggleImageOrder);    
 
     // Create a graphics object for drawing the trail line
     lineGraphics = scene.add.graphics();
 
     player = scene.physics.add.sprite(gameWidth / 2, 0, 'player');
     player.setScale(0.5);
+    player.body.collideWorldBounds = true; // Enable collision with world bounds
+
+    goon1 = scene.physics.add.sprite(gameWidth / 2, 0, 'goon1');
+    goon1.setScale(0.5);
+    goon1.setVelocity(Phaser.Math.Between(-goonSpeed, goonSpeed), Phaser.Math.Between(-goonSpeed, goonSpeed));
+    goon1.setBounce(1);
+    goon1.setCollideWorldBounds(true);
+    goon1.setAngularVelocity(goon1.body.velocity.x * goonRotationSpeed);
+
+    goon2 = scene.physics.add.sprite(gameWidth / 2, 0, 'goon2');
+    goon2.setScale(0.5);
+    goon2.setVelocity(Phaser.Math.Between(-goonSpeed, goonSpeed), Phaser.Math.Between(-goonSpeed, goonSpeed));
+    goon2.setBounce(1);
+    goon2.setCollideWorldBounds(true);
+    goon2.setAngularVelocity(goon2.body.velocity.x * goonRotationSpeed);
 
     // Enable keyboard input
     cursors = scene.input.keyboard.addKeys({
@@ -57,7 +103,7 @@ function create() {
     });
 
     // Set world bounds
-    scene.physics.world.setBoundsCollision(true, true, true, true);
+    scene.physics.world.setBounds(0, 0, gameWidth, gameHeight); // Set custom world bounds
 
     // Create UI text
     uiText = scene.add.text(10, 10, '', {
@@ -72,7 +118,6 @@ function create() {
     // Register spacebar input for pausing/resuming the game
     scene.input.keyboard.on('keydown-SPACE', togglePause);
 }
-
 
 function update() {
     // Update game logic here
@@ -138,7 +183,9 @@ function update() {
         lineGraphics.lineBetween(p1.x, p1.y, p2.x, p2.y);
     }
 
-
+    // Update goons' rotation based on velocity
+    goon1.setAngularVelocity(goon1.body.velocity.x * goonRotationSpeed);
+    goon2.setAngularVelocity(goon2.body.velocity.x * goonRotationSpeed);
 }
 
 function writeUI() {
@@ -159,4 +206,16 @@ function togglePause() {
         player.setVelocityX(0);
         player.setVelocityY(0);
     }
+}
+
+function toggleImageOrder() {
+    // Get the current depths of the background and prize images
+    var backgroundDepth = background.depth;
+    var prizeDepth = prize.depth;
+
+    // Set the background depth to the current prize depth
+    background.setDepth(prizeDepth);
+
+    // Set the prize depth to the current background depth
+    prize.setDepth(backgroundDepth);
 }
